db0d8d38579c37d0f4363760f7a554dbb5583b4d
Caleb Bertrand <developer.caleb@proton.me>
2024-09-13 14:16:26 -0500

    pattern matching

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,12 +5,18 @@
   case class Chef(override val name: String) extends Person
   case class Citizen() extends Person
 
-  val myPerson = Chef("John")
-  val myGenericPerson = Citizen()
-
   def sayPersonName[T <: Person](person: T): Unit =
     println("Hello, my name is " ++ person.name)
 
-  sayPersonName(myPerson)
-  sayPersonName(myGenericPerson)
+  val citizens = Vector(Chef("James"), Citizen(), Citizen(), Citizen())
 
+  // for comprehension
+  for citizen <- citizens if citizen.name == "James" do sayPersonName(citizen)
+
+  // Bad practice, an example of infix notation
+  citizens.filter(_.name == "James") foreach sayPersonName
+
+  // Pattern matching
+  for citizen <- citizens do citizen.name match
+    case "James" => sayPersonName(citizen)
+    case _ => // Don't print
